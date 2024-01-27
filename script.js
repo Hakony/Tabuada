8b8e536fc37bf5df7b024351bde2dfed22f7ab06
Raphael Alves <raphaalves44@gmail.com>
2024-01-27 14:10:45 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um numero!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = '' //para limpar a tabuada
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}` //mais usado em outras linguagem como PHP
+            tab.appendChild(item)
+            c++
+        }
+    }
+    
+
+}
